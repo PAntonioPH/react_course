ac65ed2b219e7ea68ad67c5a92c75e422ae14d3c
PAntonioPH <antonio.paph1@gmail.com>
2023-01-16 16:30:21 -0600

    Introducción JavaScript

---
+++ 02-intro-javascript/src/bases/10_fetch.js
@@ -0,0 +1,15 @@
+const apiKey = "DUeRvUV7LBUbA48oAm6evGiFsmmi7LLB"
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+
+peticion
+  .then((res) => res.json())
+  .then(({data}) => {
+    const {url} = data.images.original
+
+    const img = document.createElement("img")
+    img.src = url
+
+    document.body.append(img)
+  })
+  .catch(console.warn)
