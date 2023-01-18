e6c90a80caac85d6ad803325e5970e38f24bb344
PAntonioPH <antonio.paph1@gmail.com>
2023-01-18 10:11:52 -0600

    Pruebas Unitarias y de Integración

--- 03_counter_app_pruebas/test/demo.test.js
+++ 03_counter_app_pruebas/test/demo.test.js
@@ -1,5 +1,9 @@
-test("Esta prueba no debe de fallar", () => {
-  if (0 === 0) {
-    throw new Error("No se puede dividir")
-  }
+describe("Pruebas en <DemoComponent>", () => {
+  test("Esta prueba no debe de fallar", () => {
+    const message1 = "Hola mundo"
+
+    const message2 = message1.trim()
+
+    expect(message1).toBe(message2)
+  })
 })
