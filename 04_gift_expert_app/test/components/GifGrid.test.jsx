15cd461d376ac5d8b4b9c79a89a1b6a22756dcf7
PAntonioPH <antonio.paph1@gmail.com>
2023-01-27 16:53:05 -0600

    GifExpertApp Pruebas

--- 04_gift_expert_app/test/components/GifGrid.test.jsx
+++ 04_gift_expert_app/test/components/GifGrid.test.jsx
@@ -1,10 +1,18 @@
 import {render, screen} from "@testing-library/react";
 import {GifGrid} from "../../src/components/index.js";
+import {useFetchGifs} from "../../src/hooks/useFetchGifs.js";
+
+jest.mock("../../src/hooks/useFetchGifs.js")
 
 describe("Pruebas en GifGrid", () => {
   const category = "One Punch"
 
   test("Debe de mostrar el loading inicialmente", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    })
+
     render(<GifGrid category={category}/>)
 
     expect(screen.getByText("Cargando..."))
@@ -12,6 +20,26 @@ describe("Pruebas en GifGrid", () => {
   })
 
   test("Debe de mostrar item cuando se cargan las imágenes mediante", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Saitama",
+        url: "www.saitama.com"
+      },
+      {
+        id: "123",
+        title: "Saitamas",
+        url: "www.saitamas.com"
+      },
+    ]
 
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    })
+
+    render(<GifGrid category={category}/>)
+
+    expect(screen.getAllByRole("img")).toHaveLength(2)
   })
 })
