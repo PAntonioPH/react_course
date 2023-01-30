24838ef79b47aa0c7dad38b511d1242564400830
PAntonioPH <antonio.paph1@gmail.com>
2023-01-30 10:41:43 -0600

    GifExpertApp Pruebas

--- 04_gift_expert_app/test/GiftExpertApp.test.jsx
+++ 04_gift_expert_app/test/GiftExpertApp.test.jsx
@@ -1,5 +1,10 @@
+import {render, screen} from "@testing-library/react";
+import {GiftExpertApp} from "../src/GiftExpertApp.jsx";
+
 describe("Pruebas en GiftExpertApp", () => {
   test("", () => {
+    render(<GiftExpertApp/>)
 
+    screen.debug()
   })
 })
