41bae7ef5121ad2cadfb2aa2028f2ecd47b0a7e9
PAntonioPH <antonio.paph1@gmail.com>
2023-01-31 11:34:53 -0600

    CustomHook

--- 05_hook_app/src/main.jsx
+++ 05_hook_app/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
+import {MultipleCustomHooks} from "./03_examples/MultipleCustomHooks.jsx";
 // import {SimpleForm} from "./02_useEffect/SimpleForm.jsx";
-import {SimpleFormCustomHook} from "./02_useEffect/SimpleFormCustomHook.jsx";
+// import {SimpleFormCustomHook} from "./02_useEffect/SimpleFormCustomHook.jsx";
 // import {HooksApp} from "./HooksApp.jsx";
 // import {CounterApp} from "./01_useState/CounterApp.jsx";
 // import {CounterAppCustomHook} from "./01_useState/CounterAppCustomHook.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <SimpleFormCustomHook/>
+  <MultipleCustomHooks/>
   // </React.StrictMode>,
 )
