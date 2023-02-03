f46a1ec98739009118a19d08a068f441a0ce2dce
PAntonioPH <antonio.paph1@gmail.com>
2023-02-03 12:09:47 -0600

    ToDoApp

--- 05_hook_app/src/08_useReducer/toDoReducer.js
+++ 05_hook_app/src/08_useReducer/toDoReducer.js
@@ -1,11 +1,11 @@
 export const toDoReducer = (initialState = [], action) => {
   switch (action.type) {
     case "Add ToDo":
-      return [...initialState, action.payload]
+      return [...initialState, action.payload].sort((a, b) => a.done - b.done)
     case "Remove toDo":
       return initialState.filter(toDo => toDo.id !== action.payload)
     case "Toggle toDo":
-      return initialState.map(toDo => toDo.id === action.payload ? ({...toDo, done: !toDo.done}) : (toDo)).sort((a, b) => a.id - b.id).sort((a, b) => a.done - b.done)
+      return initialState.map(toDo => toDo.id === action.payload ? ({...toDo, done: !toDo.done}) : (toDo)).sort((a, b) => a.done - b.done)
     default:
       return initialState
   }
